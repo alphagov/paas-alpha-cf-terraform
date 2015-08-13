e53b43230d54ffdcf2ed13d92cd83548c1f5f63f
Dan Hilton <daniel.hilton@gmail.com>
2015-08-13 14:46:19 +0100

    Specify ssh_user for bosh-init

--- globals.tf
+++ globals.tf
@@ -6,3 +6,9 @@ variable "office_cidrs" {
   description = "CSV of CIDR addresses for our office which will be trusted"
   default     = "80.194.77.90/32,80.194.77.100/32"
 }
+
+
+variable "ssh_user" {
+	description = "Username used to ssh into VMs."
+	default     = "ubuntu"
+}
