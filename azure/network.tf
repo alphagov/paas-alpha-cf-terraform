99a00ee7c56f27f784e7ebafd6c8a916c28f9e65
Hector Rivas Gandara <keymon@gmail.com>
2015-08-25 10:47:30 +0000

    Remove azure network security group in bastion SSH
    We had multiple issues trying to setup NSG rules to restrict access to
    the SSH endpoint: failures applying multiple rules in parallel, not working
    as expected, etc.
    
    We will remove this implementation so we can try [Endpoint ACLs instead](https://azure.microsoft.com/en-gb/documentation/articles/virtual-networks-acl/)

--- azure/network.tf
+++ azure/network.tf
@@ -3,12 +3,9 @@ resource "azure_virtual_network" "default" {
   address_space = ["${var.virtual_network_cidr}"]
   location = "West Europe"
 
-  depends_on = "azure_security_group.bastion"
-
   subnet {
     name = "${var.env}-cf-bastion"
     address_prefix = "${var.bastion_cidr}"
-    security_group = "${var.env}-bastion"
   }
 
 }
