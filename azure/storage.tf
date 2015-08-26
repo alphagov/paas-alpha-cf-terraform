3b1ee797d87b8f995ea1497fe8c28c761944d5b6
Hector Rivas Gandara <keymon@gmail.com>
2015-08-26 17:12:29 +0100

    Add an azure hosted service for storage accounts
    The storage accounts, needed to be able to create storage resources in
    azure by the BOSH cpi, are associated to a `azure_hosted_service`.
    
    The `azure_hosted_service` is supported by terraform, but the account
    not, for which we have the script `azure/azure-create-storage-service.sh`.
    
    We add a dedicated service for storage with a `local_exec` provisioner
    to create the storage account.
    
    Eventually, if storage accounts are implemented in terraform, we can
    move the logic to terraform itself.

--- azure/storage.tf
+++ azure/storage.tf
@@ -4,3 +4,14 @@ resource "azure_storage_service" "cf-storage" {
     description = "Default storage for CF installation"
     account_type = "Standard_LRS"
 }
+
+resource "azure_hosted_service" "cf-storage-service" {
+    name = "${var.env}-cf-storage-service"
+    location = "West Europe"
+    ephemeral_contents = false
+    description = "Hosted service for the storage of CF."
+    label = "${var.env}-cf-storage-hs-01"
+    provisioner "local-exec" {
+        command = "./azure-create-storage-service.sh ${var.env}-cf-storage-service ${var.env}cfstgaccount generated.cf-storage-account.key"
+    }
+}
