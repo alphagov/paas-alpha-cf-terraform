16555b4845ef95fe58efb90fbad3c3aeb8f3ae9d
Hector Rivas Gandara <keymon@gmail.com>
2015-08-26 17:08:26 +0100

    Add script to create an azure storage account
    Terraform does not support creating azure storage accounts, so
    we need to use the `azure-cli` for the time being.
    
    We provide a external script `azure/azure-create-storage-service.sh`
    which can be called by `local-exec` provider.
    
    This script creates the storage account associated to a given
    service name (terraform can create those) and retrieves and
    stores the account key in a file which we can read in terraform later.
    
    If the account already exists, it just downloads the key.

---
+++ azure/azure-create-storage-service.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+if [ $# -lt 3 ]; then
+	cat <<EOF
+Usage:
+	$0 <storage service name> <storage_account_name> <account_key_file>
+
+Will create a storage service name and store the credencials on the given file
+so they can be consumed by terraform
+EOF
+	exit 1
+fi
+
+storage_service_name=$1; shift
+storage_account_name=$1; shift
+account_key_file=$1; shift
+
+set -x
+
+azure config mode arm # Change azure client mode, needed to run `azure resource create`
+
+echo "Check if the account already exists"
+echo $storage_service_name | azure storage account show  $storage_account_name
+RET=$?
+
+if [ $RET != 0 ]; then
+	echo "Creating account"
+	azure resource create $storage_service_name $storage_account_name \
+		Microsoft.Storage/storageAccounts "West Europe" \
+		2015-05-01-preview \
+		-p "{\"accountType\":\"Standard_LRS\"}"
+	# Horrible workaround. It fails once with Error:null
+	if [ $? != 0 ] && grep 'Error: null' ~/.azure/azure.err; then
+		echo "Warning: Ignoring 'Error: null' error... The account gets created anyway"
+	else
+		exit 1
+	fi
+else
+	echo "Warning: $storage_account_name account already exists in  $storage_service_name. Not creating it."
+fi
+
+echo "Retrieving the account key from the command line azure client"
+echo $storage_service_name | \
+	azure storage account keys list $storage_account_name --json > ${account_key_file}.json
+
+sed -n 's/.*"key1":.*"\(.*\)".*/\1/p' <  ${account_key_file}.json > $account_key_file || exit 1
+
+echo "Account $storage_account_name created, key in file $account_key_file."
+
