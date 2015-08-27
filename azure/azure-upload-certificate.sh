cfa58c69db7fdf1a4c95df421188bcedc499dfcc
Hector Rivas Gandara <keymon@gmail.com>
2015-08-27 18:04:50 +0100

    Fix minor bug in azure-upload-certificate.sh
    We must change the azure command line mode before running the
    required commands. Mode is changed in other scripts.

--- azure/azure-upload-certificate.sh
+++ azure/azure-upload-certificate.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 set -e
 
+azure config mode asm
+
 if azure service cert list | cat | grep -q $1-cf-bastion-service; then
 	echo "There is already a key for this service: $1-cf-bastion-service"
 else
@@ -15,8 +17,6 @@ else
 		-outform der \
 		-in generated.insecure-deployer.pem \
 		-out generated.insecure-deployer.pfx
-
-	azure config mode asm
 
 	azure service cert create $1-cf-bastion-service generated.insecure-deployer.pfx
 fi
