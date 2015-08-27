32e684eb8b4ff1efacdda20a27e7906966830743
Hector Rivas Gandara <keymon@gmail.com>
2015-08-27 11:59:02 +0100

    Keep z509 temporary ssh certs for azure
    We will use the generated x509 temporary cerficiates in other steps,
    like for instance for the BOSH manifest when passing the
    `ssh_certificate` key. Because that, we will keep the temporary files
    as `generated.insecure-deployer.pem` and
    `generated.insecure-deployer.pfx`

--- azure/azure-upload-certificate.sh
+++ azure/azure-upload-certificate.sh
@@ -5,20 +5,19 @@ openssl req -x509 \
 	-key ssh/insecure-deployer \
 	-nodes \
 	-days 365 -newkey rsa:2048 \
-	-out /tmp/insecure-deployer.pem \
+	-out generated.insecure-deployer.pem \
 	-subj '/CN=www.mydom.com/O=My Company Name LTD./C=US'
 
 openssl x509 \
 	-outform der \
-	-in /tmp/insecure-deployer.pem \
-	-out /tmp/insecure-deployer.pfx
+	-in generated.insecure-deployer.pem \
+	-out generated.insecure-deployer.pfx
 
 azure config mode asm
 
-azure service cert create $1-cf-bastion-service /tmp/insecure-deployer.pfx
+azure service cert create $1-cf-bastion-service generated.insecure-deployer.pfx
 
 azure service cert list | \
 	grep $1-cf-bastion-service | \
 	awk '{print $3}' | tr -d '\n' > generated.ssh_thumbprint
 
-rm -f /tmp/insecure-deployer.{pem,pfx}
