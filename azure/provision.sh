acacb9b0116e211eb6a8b17db61a0f2cb7a5b07b
Hector Rivas Gandara <keymon@gmail.com>
2015-08-27 17:18:17 +0100

    Optimise provision.sh on azure to reduce times
    Only install packages and download software if necesary

--- azure/provision.sh
+++ azure/provision.sh
@@ -3,8 +3,11 @@
 # Prepare the jumpbox to be able to install ruby and git-based bosh and cf repos
 cd $HOME
 
-sudo apt-get update
-sudo apt-get install -y build-essential git zlibc zlib1g-dev ruby ruby-dev openssl libxslt-dev libxml2-dev libssl-dev libreadline6 libreadline6-dev libyaml-dev libsqlite3-dev sqlite3
+PACKAGES="build-essential git zlibc zlib1g-dev ruby ruby-dev openssl libxslt-dev libxml2-dev libssl-dev libreadline6 libreadline6-dev libyaml-dev libsqlite3-dev sqlite3"
+if ! dpkg -l $PACKAGES > /dev/null 2>&1; then
+	sudo apt-get update
+	sudo apt-get install -y $PACKAGES
+fi
 
 # Set correct permissions for the ssh key we copied
 chmod 400 ~/.ssh/id_rsa
@@ -15,6 +18,9 @@ eval `ssh-agent`
 #ssh-add ~/.ssh/insecure-deployer
 ssh-add ~/.ssh/id_rsa
 
-wget https://s3.amazonaws.com/bosh-init-artifacts/bosh-init-0.0.72-linux-amd64 -O bosh-init
-chmod +x bosh-init
+if [ ! -x bosh-init ]; then
+	wget https://s3.amazonaws.com/bosh-init-artifacts/bosh-init-0.0.72-linux-amd64 -O bosh-init
+	chmod +x bosh-init
+fi
+
 ./bosh-init deploy manifest.yml
