d5502da9e3b539e819f5de65f8ffcb86beac6afd
Hector Rivas Gandara <keymon@gmail.com>
2015-08-27 16:12:13 +0100

    Fix bug in script to upload certs to azure
    `azure-upload-certificate.sh` was not handling properly the case
    that there is already a certificate uploaded.

--- azure/azure-upload-certificate.sh
+++ azure/azure-upload-certificate.sh
@@ -1,23 +1,27 @@
 #!/bin/sh
 set -e
 
-openssl req -x509 \
-	-key ssh/insecure-deployer \
-	-nodes \
-	-days 365 -newkey rsa:2048 \
-	-out generated.insecure-deployer.pem \
-	-subj '/CN=www.mydom.com/O=My Company Name LTD./C=US'
+if azure service cert list | cat | grep -q $1-cf-bastion-service; then
+	echo "There is already a key for this service: $1-cf-bastion-service"
+else
+	openssl req -x509 \
+		-key ssh/insecure-deployer \
+		-nodes \
+		-days 365 -newkey rsa:2048 \
+		-out generated.insecure-deployer.pem \
+		-subj '/CN=www.mydom.com/O=My Company Name LTD./C=US'
 
-openssl x509 \
-	-outform der \
-	-in generated.insecure-deployer.pem \
-	-out generated.insecure-deployer.pfx
+	openssl x509 \
+		-outform der \
+		-in generated.insecure-deployer.pem \
+		-out generated.insecure-deployer.pfx
 
-azure config mode asm
+	azure config mode asm
 
-azure service cert create $1-cf-bastion-service generated.insecure-deployer.pfx
+	azure service cert create $1-cf-bastion-service generated.insecure-deployer.pfx
+fi
 
 azure service cert list | \
 	grep $1-cf-bastion-service | \
-	awk '{print $3}' | tr -d '\n' > generated.ssh_thumbprint
+	awk '{print $3}' | head -n 1 | tr -d '\n' > generated.ssh_thumbprint
 
