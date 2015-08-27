fef5cdd70e67379fab8909c08524e782a57b191b
Hector Rivas Gandara <keymon@gmail.com>
2015-08-27 18:25:21 +0100

    Print IP after creation in azure-create-public-ip.sh
    Also sleep before querying the IP in case Azure did not finish creating it.

--- azure/azure-create-public-ip.sh
+++ azure/azure-create-public-ip.sh
@@ -23,9 +23,12 @@ azure resource create \
      "West Europe" 2015-05-01-preview \
      -p "{\"publicIPAllocationMethod\":\"static\"}"
 
+sleep 5
+
 azure resource show \
      ${resource_group_name} \
      ${ip_name} \
      Microsoft.Network/publicIPAddresses \
      2015-05-01-preview | awk '/Property ipAddress/ {print $4}' | tee generated.${ip_name}
 
+echo "Created IP $(<generated.${ip_name}), stored in generated.${ip_name}"
