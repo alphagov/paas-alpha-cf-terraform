f23d983ca1a88e24dbf5ee5e544027c5c43d5611
Hector Rivas Gandara <keymon@gmail.com>
2015-08-27 17:44:39 +0100

    Add a script to allocate public IPs in azure
    Terraform does not support create public IPs, so we need to use the
    azure client for the time being.
    
    We will need a public IP for microbosh to be able to contact it
    meanwhile we create it in a different network than the bastion host [due
    the limitations in
    terraform](https://github.com/hashicorp/terraform/issues/3089)

---
+++ azure/azure-create-public-ip.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+set -e
+
+if [ $# -lt 2 ]; then
+	cat <<EOF
+Usage:
+	$0 <resource_group_name> <ip_name>
+
+Creates a public IP
+EOF
+	exit 1
+fi
+
+resource_group_name=$1; shift
+ip_name=$1; shift
+
+azure config mode arm
+azure resource create \
+     ${resource_group_name} \
+     ${ip_name} \
+     Microsoft.Network/publicIPAddresses \
+     "West Europe" 2015-05-01-preview \
+     -p "{\"publicIPAllocationMethod\":\"static\"}"
+
+azure resource show \
+     ${resource_group_name} \
+     ${ip_name} \
+     Microsoft.Network/publicIPAddresses \
+     2015-05-01-preview | awk '/Property ipAddress/ {print $4}' | tee generated.${ip_name}
+
