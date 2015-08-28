a2957a59095bc9bbf3871a3e63839be237169f19
Hector Rivas Gandara <keymon@gmail.com>
2015-08-28 15:46:22 +0100

    Add output for bastion_ip in azure
    It is required for the Makefile tasks to provision

--- azure/output.tf
+++ azure/output.tf
@@ -1,9 +1,11 @@
 output "bastion-vips" {
   value = "${join(",", azure_instance.bastion.*.vip_address)}"
 }
-
 output "bastion-ips" {
   value = "${join(",", azure_instance.bastion.*.ip_address)}"
+}
+output "bastion_ip" {
+  value = "${azure_instance.bastion.0.vip_address}"
 }
 
 #output "manifest" {
