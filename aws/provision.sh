83f8bdb4012f1ac83253de81bbe44a216220fc0a
Hector Rivas Gandara <keymon@gmail.com>
2015-09-02 11:35:54 +0100

    Fix error handling and optimise `aws/provision.sh`
    Check if some resources are already present, update
    the cf-release, etc.

--- aws/provision.sh
+++ aws/provision.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 STEMCELL=bosh-stemcell-3056-aws-xen-ubuntu-trusty-go_agent.tgz
 
-# Prepare the jumpbox to be able to install ruby and git-based bosh and cf repos
-sudo apt-get update
-sudo apt-get install -y build-essential git zlibc zlib1g-dev ruby ruby-dev openssl libxslt-dev libxml2-dev libssl-dev libreadline6 libreadline6-dev libyaml-dev libsqlite3-dev sqlite3 dstat
+PACKAGES="build-essential git zlibc zlib1g-dev ruby ruby-dev openssl libxslt-dev libxml2-dev libssl-dev libreadline6 libreadline6-dev libyaml-dev libsqlite3-dev sqlite3 dstat"
+if ! dpkg -l $PACKAGES > /dev/null 2>&1; then
+	# Prepare the jumpbox to be able to install ruby and git-based bosh and cf repos
+  sudo apt-get update
+	sudo apt-get install -y $PACKAGES
+fi
 
 # Set correct permissions for the ssh key we copied
 # TF feature request: https://github.com/hashicorp/terraform/issues/3071
@@ -22,8 +25,13 @@ export BOSH_INIT_LOG_PATH=bosh_init.log
 time ./bosh-init deploy manifest_aws.yml
 
 # TODO: install bosh cli rubygems in parallel to bosh-init (which mainly stresses bosh server)
-echo "Installing bosh cli..."
-time sudo gem install bosh_cli -v 1.3056.0 --no-ri --no-rdoc
+if gem list | grep -q bosh_cli; then
+  echo "Bosh cli already installed, skipping..."
+else
+  echo "Installing bosh cli..."
+  time sudo gem install bosh_cli -v 1.3056.0 --no-ri --no-rdoc
+fi
+
 export PATH=$PATH:/usr/local/bin/bosh
 
 # FIXME: make passwords properly strong
@@ -32,11 +40,15 @@ export PATH=$PATH:/usr/local/bin/bosh
 echo -e "admin\nadmin" | bosh target 10.0.0.6 "IamIgnored"
 
 # TODO: download stemcell from our own bucket using multiple threads, in paralell with other tasks
-time bosh download public stemcell $STEMCELL
-time bosh upload stemcell $STEMCELL
+if [ ! -f $STEMCELL ]; then
+  time bosh download public stemcell $STEMCELL
+fi
+time bosh upload stemcell $STEMCELL --skip-if-exists
 
 # TODO: this can also happen in parallel
-time git clone https://github.com/cloudfoundry/cf-release.git -b v215
+if [ ! -d cf-release ]; then
+  time git clone https://github.com/cloudfoundry/cf-release.git
+fi
 
 # TODO: pre-seed bosh cache, otherwise action below takes forever...
 # FIXME: make output from bosh command stream and have color. It takes quite some time to complete, immediate output is desirable.
@@ -45,4 +57,6 @@ time git clone https://github.com/cloudfoundry/cf-release.git -b v215
 # No, you can't bosh upload release cf-release/releases/cf-215.yml, you have to CD into the cf-release first
 echo "Uploading v215 release to bosh..."
 cd cf-release
+git checkout v215
+./update
 time bosh upload release releases/cf-215.yml
