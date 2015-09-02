a066c7217b065dcd30702d44d1ab724b8b0ab2f8
Michal Tekel <mtekel@gmail.com>
2015-09-02 16:41:02 +0100

    Add fixed external IP for haproxy (and CF)
    Reserve external fixed IP address in GCE to be used for haproxy. Haproxy is the entry point for CF, so the CF API and Apps will be reachable on this IP.

--- gce/outputs.tf
+++ gce/outputs.tf
@@ -1,6 +1,11 @@
 output "bastion_ip" {
   value = "${google_compute_instance.bastion.network_interface.0.access_config.0.nat_ip}"
 }
+
 output "bosh_ip" {
 	value = "${google_compute_address.bosh.address}"
+}
+
+output "haproxy_ip" {
+        value = "${google_compute_address.haproxy.address}"
 }
