2eb8e8ac6e7783a1ddf8a68a798fbc64740aeee7
Michal Tekel <mtekel@gmail.com>
2015-09-08 17:19:05 +0100

    GCE provision: handle route creation
    Add microbosh internal route handling into the provision script. This route re-routes all the traffic to the microbosh external IP via the internal network. Currently we always delete the route first, as it prevents bosh-init configuring the microbosh, because the bosh-init always creates the new VM to apply any changes. The old route would then route traffic to blackhole (old VM, which just bosh-init deleted) and won't let bosh-init connect to new instance to finish configuration. There is a story to handle this better and only delete the route when the VM gets re-created. We also check if route is still correct and in case any of its parameters have been changed, we correct them - by re-creating the route. This logic can be probably split up into it's own script when working on unification of GCE and other cloud provider provisioning scripts.
    
    On the microbosh server, we configure PREROUTING and OUTPUT iptables rules. PREROUTING translates bosh external IP to the internal eth0 IP, replicating what google 1:1 NAT does. The OUTPUT rule translates all outgoing traffic to external mBosh IP back to eth0. This avoids traffic attempting to go via google routing, which for this instance doesn't really work - there's a route for the external IP pointing back to mBosh (which works correctly for all other instances, but this one) and at the same time there's likely NAT being activated. The packets sent to external mBosh IP never return back. Thus we rewrite them explicitly to eth0 IP, replicating what GCE 1:1 NAT does for externally incoming connections.

--- gce/provision.sh
+++ gce/provision.sh
@@ -2,6 +2,19 @@
 STEMCELL=light-bosh-stemcell-2968-google-kvm-ubuntu-trusty-go_agent.tgz
 RELEASE=210
 BOSH_EXTERNAL_IP=$1
+MICROBOSH_ZONE=europe-west1-b
+DEPLOYMENT_NAME=`python -c 'import yaml; print yaml.load(file("cf-manifest.yml"))["name"]'`
+
+# Returns the $2 field from $1 file, with $3 extra syntax
+json_get(){
+  value=`python -c "import json; print json.load(file(\"$1\"))[\"$2\"]$3"`
+  if [[ $? != 0 ]] ; then
+    echo "Error retrieving $2 from $1"
+    exit 100
+  else
+    echo "$value"
+  fi
+}
 
 # Prepare the jumpbox to be able to install ruby and git-based bosh and cf repos
 PACKAGES="build-essential git zlibc zlib1g-dev ruby ruby-dev openssl libxslt-dev libxml2-dev libssl-dev libreadline6 libreadline6-dev libyaml-dev libsqlite3-dev sqlite3 dstat"
@@ -22,6 +35,16 @@ tr -d '\n' < account.json > account_tmp.json
 python -c 'print open("manifest_gce.yml").read().replace("ACCOUNT_JSON", open("account_tmp.json").read()).rstrip().rstrip("EOF")' > microbosh-manifest.yml 2>&1
 rm account_tmp.json manifest_gce.yml
 
+# Login to GCE
+export CLOUDSDK_PYTHON_SITEPACKAGES=1
+ACCOUNT=`json_get account.json client_email`
+json_get account.json private_key > gce.key && chmod 600 gce.key
+gcloud auth activate-service-account $ACCOUNT --key-file gce.key
+
+# TODO: Add logic that checks if bosh-init really does re-create mBosh VM and only remove the route then
+echo "Attempting to delete $DEPLOYMENT_NAME-internalbosh route..."
+gcloud compute routes delete -q $DEPLOYMENT_NAME-internalbosh
+
 # Deploy microbosh server
 if [ ! -x bosh-init ]; then
   wget https://s3.amazonaws.com/bosh-init-artifacts/bosh-init-0.0.72-linux-amd64 -O bosh-init
@@ -30,6 +53,64 @@ fi
 export BOSH_INIT_LOG_LEVEL=debug
 export BOSH_INIT_LOG_PATH=bosh-init.log
 time ./bosh-init deploy microbosh-manifest.yml
+
+# Configure internal routing for microbosh
+# 1. Get microbosh IP
+BOSH_VM=`json_get microbosh-manifest-state.json current_vm_cid`
+gcloud compute instances describe --zone $MICROBOSH_ZONE --format json $BOSH_VM > microbosh-info.json
+BOSH_INTERNAL_IP=`json_get microbosh-info.json networkInterfaces '[0]["networkIP"]'`
+
+# 2. Configure iptables on microbosh server
+echo "Configuring IPtables on the microbosh..."
+RULE="-d $BOSH_EXTERNAL_IP/32 -j DNAT --to-destination $BOSH_INTERNAL_IP"
+ssh -T -oStrictHostKeyChecking=no $BOSH_INTERNAL_IP <<EOF
+sudo DEBIAN_FRONTEND=noninteractive apt-get install -y iptables-persistent
+for CHAIN in PREROUTING OUTPUT ; do
+  echo "Checking \$CHAIN $RULE"
+  sudo iptables -t nat -C \$CHAIN $RULE || sudo iptables -t nat -A \$CHAIN $RULE
+done
+sudo service iptables-persistent save
+EOF
+
+# 3. Re-route BOSH_EXTERNAL_IP internally. Check if route exists and is correct first.
+CREATE=false
+UPDATE=false
+gcloud compute routes describe $DEPLOYMENT_NAME-internalbosh --format json >internalbosh-route.json 2>route.errors
+if [[ $? != 0 ]] ; then
+  grep -q "The resource 'projects/.\+/routes/${DEPLOYMENT_NAME}-internalbosh' was not found" route.errors
+  if [[ $? == 0 ]] ; then
+    CREATE=true
+  else
+    echo "Failed retrieving ${DEPLOYMENT_NAME}-internalbosh route information, aborting. Errors:"
+    cat route.errors
+    exit 255
+  fi
+else
+  # Compare if any of the route attributes need updating
+  destRange=`json_get internalbosh-route.json destRange`
+   priority=`json_get internalbosh-route.json priority`
+    network=`json_get internalbosh-route.json network '.split("/")[-1]'`
+  routedest=`json_get internalbosh-route.json nextHopInstance '.split("/")[-1]'`
+
+  [[ "$destRange" != "$BOSH_EXTERNAL_IP/32" ]] && UPDATE=true
+  [[ "$priority"  != "1" ]] && UPDATE=true
+  [[ "$network"   != "$DEPLOYMENT_NAME-cf-bastion" ]] && UPDATE=true
+  [[ "$routedest" != "$BOSH_VM" ]] && UPDATE=true
+
+  # In GCE, you can't update routes, you have to delete and create new
+  if $UPDATE ; then
+    echo "Route needs updating, deleting..."
+    gcloud compute routes delete -q $DEPLOYMENT_NAME-internalbosh
+    [[ $? != 0 ]] && echo "Need to update the route, but failed to delete, aborting" && exit 102
+    CREATE=true
+  fi
+fi
+
+if $CREATE ; then
+  echo "Creating $DEPLOYMENT_NAME-internalbosh route..."
+  gcloud compute routes create $DEPLOYMENT_NAME-internalbosh --network $DEPLOYMENT_NAME-cf-bastion --destination-range "$BOSH_EXTERNAL_IP/32" --next-hop-instance $BOSH_VM --next-hop-instance-zone $MICROBOSH_ZONE --priority 1 --description Route_packets_for_bosh_external_IP_directly_to_the_microbosh_instance_via_internal_network
+  [[ $? != 0 ]] && echo "Failed creating route, aborting" && exit 103
+fi
 
 # Install BOSH CLI and log in
 if gem list | grep -q bosh_cli; then
