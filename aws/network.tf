6fd8d81f1749c5ea883255702a6549a62fb8e6dd
Richard Knop <risoknop@gmail.com>
2015-09-10 11:03:25 +0100

    Let's have two subnets. Private and public (for NAT).

--- aws/network.tf
+++ aws/network.tf
@@ -6,17 +6,6 @@ resource "aws_internet_gateway" "default" {
   vpc_id = "${aws_vpc.default.id}"
 }
 
-resource "aws_subnet" "bastion" {
-  count             = 1
-  vpc_id            = "${aws_vpc.default.id}"
-  cidr_block        = "${lookup(var.public_cidrs, concat("zone", count.index))}"
-  availability_zone = "${lookup(var.zones, concat("zone", count.index))}"
-  depends_on = ["aws_internet_gateway.default"]
-  tags {
-    Name = "${var.env}-bastion-subnet-${count.index}"
-  }
-}
-
 resource "aws_route_table" "internet" {
   vpc_id = "${aws_vpc.default.id}"
   route {
@@ -25,8 +14,37 @@ resource "aws_route_table" "internet" {
   }
 }
 
-resource "aws_route_table_association" "bastion" {
+resource "aws_subnet" "private" {
+  count             = 1
+  vpc_id            = "${aws_vpc.default.id}"
+  cidr_block        = "${lookup(var.private_cidrs, concat("zone", count.index))}"
+  availability_zone = "${lookup(var.zones, concat("zone", count.index))}"
+  depends_on = ["aws_internet_gateway.default"]
+  tags {
+    Name = "${var.env}-private-subnet-${count.index}"
+  }
+}
+
+resource "aws_route_table_association" "private" {
   count = 1
-  subnet_id = "${element(aws_subnet.bastion.*.id, count.index)}"
+  subnet_id = "${element(aws_subnet.private.*.id, count.index)}"
+  route_table_id = "${aws_route_table.internet.id}"
+}
+
+resource "aws_subnet" "public" {
+  count             = 1
+  vpc_id            = "${aws_vpc.default.id}"
+  cidr_block        = "${lookup(var.public_cidrs, concat("zone", count.index))}"
+  availability_zone = "${lookup(var.zones, concat("zone", count.index))}"
+  map_public_ip_on_launch = true
+  depends_on = ["aws_internet_gateway.default"]
+  tags {
+    Name = "${var.env}-tsuru-public-subnet-${count.index}"
+  }
+}
+
+resource "aws_route_table_association" "public" {
+  count = 1
+  subnet_id = "${element(aws_subnet.public.*.id, count.index)}"
   route_table_id = "${aws_route_table.internet.id}"
 }
