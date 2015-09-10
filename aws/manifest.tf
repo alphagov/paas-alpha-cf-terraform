7dd9b4f37d83c557f37b653863e444b5a258e608
Richard Knop <risoknop@gmail.com>
2015-09-10 14:12:20 +0100

    Renamed cf-manifest.yml to cf-stub.yml

--- aws/manifest.tf
+++ aws/manifest.tf
@@ -9,9 +9,9 @@ resource "template_file" "manifest" {
     filename = "${path.module}/manifest.yml.tpl"
 
     vars {
-        aws_static_ip           = "${var.microbosh_IP}"
+        aws_static_ip           = "10.128.10.6"
         aws_public_ip           = "${aws_eip.bosh.public_ip}"
-        aws_subnet_id           = "${aws_subnet.bastion.0.id}"
+        aws_subnet_id           = "${aws_subnet.public.0.id}"
         aws_availability_zone   = "${var.zones.zone0}"
         aws_secret_access_key   = "${var.AWS_SECRET_ACCESS_KEY}"
         aws_access_key_id       = "${var.AWS_ACCESS_KEY_ID}"
@@ -25,18 +25,25 @@ resource "template_file" "manifest" {
     }
 }
 
-resource "template_file" "cf_manifest" {
-    filename = "${path.module}/cf-manifest.yml.tpl"
+resource "template_file" "cf_stub" {
+    filename = "${path.module}/cf-stub.yml.tpl"
 
     vars {
-        aws_subnet_id           = "${aws_subnet.bastion.0.id}"
-        aws_availability_zone   = "${var.zones.zone0}"
-        default_security_group  = "${aws_security_group.bosh_vm.name}"
+        environment             = "${var.env}"
+        zone0                   = "${var.zones.zone0}"
+        zone1                   = "${var.zones.zone1}"
+        region                  = "${var.region}"
+        public_subnet_id        = "${aws_subnet.public.0.id}"
+        private_subnet_id       = "${aws_subnet.private.0.id}"
+        aws_secret_access_key   = "${var.AWS_SECRET_ACCESS_KEY}"
+        aws_access_key_id       = "${var.AWS_ACCESS_KEY_ID}"
+        ccdb_address            = "${aws_db_instance.ccdb.address}"
+        uaadb_address           = "${aws_db_instance.uaadb.address}"
+        router0                 = "${aws_instance.router.0.id}"
+        router1                 = "${aws_instance.router.1.id}"
     }
 
     provisioner "local-exec" {
-      command = "echo '${template_file.cf_manifest.rendered}' > cf-manifest.yml"
+      command = "echo '${template_file.cf_stub.rendered}' > cf-stub.yml"
     }
 }
-
-
