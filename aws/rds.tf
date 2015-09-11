db204f687e9856ad5889026172529a20b44b4656
Richard Knop <risoknop@gmail.com>
2015-09-11 16:20:36 +0100

    Defined RDS db subnet and a security group to allow 5432.

--- aws/rds.tf
+++ aws/rds.tf
@@ -1,3 +1,12 @@
+resource "aws_db_subnet_group" "cf_rds_subnet" {
+    name = "${var.env}-cf-rds-subnet"
+    description = "Subnet group for RDS"
+    subnet_ids = [
+      "${aws_subnet.private.id}",
+      "${aws_subnet.public.id}"
+    ]
+}
+
 resource "aws_db_instance" "uaadb" {
     identifier = "${var.env}-uaadb-rds"
     allocated_storage = 10
@@ -7,6 +16,8 @@ resource "aws_db_instance" "uaadb" {
     name = "uaadb"
     username = "uaadb"
     password = "uaadbpassword"
+    db_subnet_group_name = "${var.env}-cf-rds-subnet"
+    vpc_security_group_ids = ["${aws_security_group.rds.id}"]
 }
 
 resource "aws_db_instance" "ccdb" {
@@ -18,4 +29,26 @@ resource "aws_db_instance" "ccdb" {
     name = "ccdb"
     username = "ccdb"
     password = "ccdbpassword"
+    db_subnet_group_name = "${var.env}-cf-rds-subnet"
+    vpc_security_group_ids = ["${aws_security_group.rds.id}"]
+}
+
+resource "aws_security_group" "rds" {
+  name = "${var.env}-rds"
+  description = "RDS security group"
+  vpc_id = "${aws_vpc.default.id}"
+
+  ingress {
+    from_port = 5432
+    to_port   = 5432
+    protocol  = "tcp"
+    security_groups = [
+      "${aws_security_group.director.id}",
+      "${aws_security_group.bosh_vm.id}"
+    ]
+  }
+
+  tags {
+    Name = "${var.env}-rds"
+  }
 }
