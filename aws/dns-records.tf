a527539acfbd4be93cfe0a8f945d91b5ddff9604
Richard Knop <risoknop@gmail.com>
2015-09-11 17:21:58 +0100

    Removed -router from DNS record.

--- aws/dns-records.tf
+++ aws/dns-records.tf
@@ -1,6 +1,6 @@
 resource "aws_route53_record" "wildcard" {
   zone_id = "${var.dns_zone_id}"
-  name = "*.${var.env}-router.${var.dns_zone_name}"
+  name = "*.${var.env}.${var.dns_zone_name}"
   type = "CNAME"
   ttl = "60"
   records = ["${aws_elb.router.dns_name}"]
