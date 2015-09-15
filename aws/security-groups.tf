dd44490217c4c54a0e79d038ed96c498b6cb20ea
Hector Rivas Gandara <keymon@gmail.com>
2015-09-15 14:34:16 +0100

    Move AWS firewall.tf to security-groups.tf
    In order to keep consistency and all related concepts together, we
    move all the security groups to the same file: `aws/security-groups.tf`.
    
    This is the same logic we follow in tsuru-terraform.

--- aws/security-groups.tf
+++ aws/security-groups.tf
@@ -70,3 +70,126 @@ resource "aws_security_group" "web" {
     Name = "${var.env}-cf-web"
   }
 }
+
+resource "aws_security_group" "bastion" {
+  name = "${var.env}-bastion"
+  description = "Security group for bastion that allows SSH traffic from the office"
+  vpc_id = "${aws_vpc.default.id}"
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port = 22
+    to_port   = 22
+    protocol  = "tcp"
+    cidr_blocks = ["${split(",", var.office_cidrs)}"]
+  }
+
+  tags {
+    Name = "${var.env}-bastion"
+  }
+}
+
+resource "aws_security_group" "director" {
+  name = "${var.env}-director"
+  description = "Microbosh security group"
+  vpc_id = "${aws_vpc.default.id}"
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port = 22
+    to_port   = 22
+    protocol  = "tcp"
+    security_groups = [
+      "${aws_security_group.bastion.id}",
+    ]
+  }
+
+  ingress {
+    from_port = 4222
+    to_port   = 4222
+    protocol  = "tcp"
+    security_groups = [
+      "${aws_security_group.bastion.id}",
+      "${aws_security_group.bosh_vm.id}",
+    ]
+  }
+
+  ingress {
+    from_port = 6868
+    to_port   = 6868
+    protocol  = "tcp"
+    security_groups = [
+      "${aws_security_group.bastion.id}",
+    ]
+  }
+
+  ingress {
+    from_port = 25250
+    to_port   = 25250
+    protocol  = "tcp"
+    security_groups = [
+      "${aws_security_group.bastion.id}",
+      "${aws_security_group.bosh_vm.id}",
+    ]
+  }
+
+  ingress {
+    from_port = 25555
+    to_port   = 25555
+    protocol  = "tcp"
+    security_groups = [
+      "${aws_security_group.bastion.id}",
+    ]
+  }
+
+  ingress {
+    from_port = 25777
+    to_port   = 25777
+    protocol  = "tcp"
+    security_groups = [
+      "${aws_security_group.bastion.id}",
+      "${aws_security_group.bosh_vm.id}",
+    ]
+  }
+
+  tags {
+    Name = "${var.env}-director"
+  }
+}
+
+resource "aws_security_group" "bosh_vm" {
+  name = "${var.env}-bosh-vm"
+  description = "Security group for VMs managed by Bosh"
+  vpc_id = "${aws_vpc.default.id}"
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port = 0
+    to_port   = 0
+    protocol  = "-1"
+    cidr_blocks = ["${var.vpc_cidr}"]
+  }
+
+  tags {
+    Name = "${var.env}-bosh-vm"
+  }
+}
+
