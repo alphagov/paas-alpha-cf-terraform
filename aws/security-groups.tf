380ae161ccc390157fad04ab04ac5bed71880253
Hector Rivas Gandara <keymon@gmail.com>
2015-09-15 14:45:01 +0100

    Remove nat, cf_route and default AWS securitygroup
    We have decided not to setup bastion as a NAT host **for the time
    being**, and instead use first public IPs for all the VMs, later setup
    nat in one go.
    
    Because that, we do not need all the security groups copied from the
    tsuru configuration. This simplifies the configuration as we only keep
    these security groups: bastion, director, bosh_vms and web.

--- aws/security-groups.tf
+++ aws/security-groups.tf
@@ -1,31 +1,3 @@
-resource "aws_security_group" "default" {
-  name = "${var.env}-default-cf"
-  description = "Default security group which allows SSH access and outbound traffic"
-  vpc_id = "${aws_vpc.default.id}"
-
-  # Allow inbound SSH access
-  ingress {
-    from_port       = 22
-    to_port         = 22
-    protocol        = "tcp"
-    security_groups = [
-      "${aws_security_group.nat.id}"
-    ]
-  }
-
-  # Allow outbound traffic via NAT box
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  tags {
-    Name = "${var.env}-cf-default"
-  }
-}
-
 resource "aws_security_group" "web" {
   name = "${var.env}-web-cf"
   description = "Security group for web that allows web traffic from internet"
