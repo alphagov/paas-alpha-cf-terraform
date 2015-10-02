1ac3b5ce1a6d9473a436ed61f3e4de32fec6351b
Martin Jackson <mjackson@equalexperts.com>
2015-10-02 16:17:30 +0100

    Turn off public IP mapping for CF VMs
    In this commit we have done the following:
    
    * Disabled public IP mapping for each Cloud Foundry Virtual Machine
    * Set the default route for all CF VMs to be via bastion instance
    * Create a firewall rule to allow bosh deployed CF vms to route via bastion
    * Changed the tag for CF VMs to be `cf` instead of `bosh` because
      we use DNAT for the microbosh to allow routing to bastion and this
      wll not work if the routing rule is applied to bosh

--- gce/network.tf
+++ gce/network.tf
@@ -6,3 +6,13 @@ resource "google_compute_network" "bastion" {
 resource "google_compute_address" "bosh" {
     name = "${var.env}-bosh"
 }
+
+resource "google_compute_route" "private_default" {
+  name = "${var.env}-private-default"
+  dest_range = "0.0.0.0/0"
+  network = "${google_compute_network.bastion.name}"
+  next_hop_instance = "${google_compute_instance.bastion.name}"
+  next_hop_instance_zone = "${google_compute_instance.bastion.zone}"
+  priority = 1
+  tags = [ "cf" ]
+}
