401014889c0fa052ee906f8a64811eb039a6252a
Michal Tekel <mtekel@gmail.com>
2015-10-09 12:29:38 +0100

    provision: Use $CF_VERSION for cf version in bosh
    We used it in the repository url, but not the bosh release version.

--- scripts/provision.sh
+++ scripts/provision.sh
@@ -38,7 +38,7 @@ CF_RELEASE_REVISION=gds-paas
 
 # Releases to upload
 BOSH_RELEASES="
-cf,215,https://bosh.io/d/github.com/cloudfoundry/cf-release?v=$CF_RELEASE
+cf,$CF_RELEASE,https://bosh.io/d/github.com/cloudfoundry/cf-release?v=$CF_RELEASE
 elasticsearch,0.1.0,https://github.com/hybris/elasticsearch-boshrelease/releases/download/v0.1.0/elasticsearch-0.1.0.tgz
 graphite,0d79bf5aa5f2cf29195bff725d7dee55dea1aedc,https://github.com/CloudCredo/graphite-statsd-boshrelease.git,create
 collectd,ec9de5dc63715237688c3b27154c86a0c22b3aef,https://github.com/alphagov/collectd-graphite-boshrelease.git,create
