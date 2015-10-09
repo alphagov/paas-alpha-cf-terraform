f4aa434b2f0993d53dfe96f3ee69a8b3ff3f923f
Hector Rivas Gandara <keymon@gmail.com>
2015-10-09 12:15:54 +0100

    Add script to generate the logsearch manifest
    Make will upload it, but currently it does not deploy it.
    
    To deploy, run this on the bastion after deploying CF:
    
    ```
    ./generate_logsearch_manifest.sh aws > logsearch_manifest.yml
    bosh deployment logsearch_manifest.yml
    bosh deploy
    bosh run errand push-kibana
    ```
    
    Access the board in:
    
    https://logs.hector051015.cf.paas.alphagov.co.uk/

---
+++ manifests/generate_logsearch_manifest.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+templates=$(dirname $0)/templates
+
+infrastructure=$1
+
+shift
+
+if [ "$infrastructure" != "aws" ] && \
+    [ "$infrastructure" != "warden" ] && \
+    [ "$infrastructure" != "vsphere" ] ; then
+  echo "usage: ./generate_deployment_manifest <aws|warden|vsphere> [stubs...]"
+  exit 1
+fi
+
+spiff merge \
+  $templates/logsearch/logsearch-deployment.yml \
+  $templates/logsearch/logsearch-filters.yml \
+  $templates/logsearch/logsearch-jobs.yml \
+  $templates/logsearch/logsearch-infrastructure-${infrastructure}.yml \
+  $templates/logsearch/stub.yml \
+  $templates/stubs/director-uuid.yml \
+  $templates/outputs/terraform-outputs-${infrastructure}.yml \
+  $templates/cf-secrets.yml \
+  "$@"
