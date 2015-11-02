cceb706b0ecfeac484cb7ceb2d9e21d92e84d7cd
Hector Rivas Gandara <keymon@gmail.com>
2015-11-02 17:10:54 +0000

    Add DNS entries for GCE bastion and bosh

--- gce/dns-records.tf
+++ gce/dns-records.tf
@@ -5,3 +5,20 @@ resource "google_dns_record_set" "wildcard" {
   ttl = "60"
   rrdatas = ["${google_compute_forwarding_rule.router_https.ip_address}"]
 }
+
+resource "google_dns_record_set" "bastion" {
+  managed_zone = "${var.dns_zone_id}"
+  name = "${var.env}-bastion.${var.dns_zone_name}."
+  type = "A"
+  ttl = "60"
+  rrdatas = ["${google_compute_instance.bastion.network_interface.0.access_config.0.nat_ip}"]
+}
+
+resource "google_dns_record_set" "bosh" {
+  managed_zone = "${var.dns_zone_id}"
+  name = "${var.env}-bosh.${var.dns_zone_name}."
+  type = "A"
+  ttl = "60"
+  rrdatas = ["${google_compute_address.bosh.address}"]
+}
+
