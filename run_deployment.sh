6897adbdcb4c33fff399024f567ef60054a8e05c
Piotr Komborski <piotr.komborski@gmail.com>
2015-11-18 15:02:54 +0000

    Removed unused code

--- run_deployment.sh
+++ run_deployment.sh
@@ -1,16 +1,9 @@
 #! /bin/bash
 
-export TF_VAR_AWS_ACCESS_KEY_ID=$1
-export TF_VAR_AWS_SECRET_ACCESS_KEY=$2
+env 
 
 DIR=$(dirname $0)
 cd $DIR
-
-#make aws DEPLOY_ENV=piotr
-env
-#!/bin/bash
-
-echo Set-up ssh keys and credentials
 
 echo Setting up ssh-agent and cleanup trap
 echo $SSH_KEY > ~/.ssh/insecure-deployer
@@ -19,4 +12,4 @@ eval $(ssh-agent) && ssh-add ~/.ssh/insecure-deployer
 set -e
 trap "kill ${SSH_AGENT_PID}" ERR
 
-make aws DEPLOY_ENV=${DEPLOY_ENV} ${EXTRA_OPTIONS}
+make aws DEPLOY_ENV=piotr ${EXTRA_OPTIONS}
