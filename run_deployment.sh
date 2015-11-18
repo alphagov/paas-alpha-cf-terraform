09178b0028e3260e423076b90f53bdf0041c75f0
Piotr Komborski <piotr.komborski@gmail.com>
2015-11-18 14:51:25 +0000

    Add ssh agent to pipeline

--- run_deployment.sh
+++ run_deployment.sh
@@ -8,3 +8,15 @@ cd $DIR
 
 #make aws DEPLOY_ENV=piotr
 env
+#!/bin/bash
+
+echo Set-up ssh keys and credentials
+
+echo Setting up ssh-agent and cleanup trap
+echo $SSH_KEY > ~/.ssh/insecure-deployer
+chmod 400 ~/.ssh/insecure-deployer
+eval $(ssh-agent) && ssh-add ~/.ssh/insecure-deployer
+set -e
+trap "kill ${SSH_AGENT_PID}" ERR
+
+make aws DEPLOY_ENV=${DEPLOY_ENV} ${EXTRA_OPTIONS}
