495c6664805f5061d2e971c83fc9b22d1df9e030
Alex Tomlins <alex.tomlins@digital.cabinet-office.gov.uk>
2015-11-18 14:01:15 +0000

    Hook ingo bosh-cli to do some minimal validation of the manifest.

--- tests/spec/sample_spec.rb
+++ tests/spec/sample_spec.rb
@@ -1,10 +1,37 @@
 require 'yaml'
+require 'cli'
 
 RSpec.describe "some sample tests" do
+  let(:manifest_file) { File.expand_path("../../../cf-manifest.yml", __FILE__) }
+  let(:manifest) { YAML.load_file(manifest_file) }
 
-  let(:manifest) {
-    YAML.load_file(File.expand_path("../../../cf-manifest.yml", __FILE__))
+  let(:mock_bosh_releases) {
+    [
+      { "name"=>"cf", "release_versions"=>[{"version"=>"215"}] },
+      { "name"=>"cf-redis", "release_versions"=>[{"version"=>"420"}] },
+      { "name"=>"collectd", "release_versions"=>[{"version"=>"ec9de5dc63715237688c3b27154c86a0c22b3aef"}] },
+      { "name"=>"elasticsearch", "release_versions"=>[{"version"=>"0.1.0"}] },
+      { "name"=>"grafana", "release_versions"=>[{"version"=>"44564533c9d4d656bdcd5633b808f0bf6fb177ae"}] },
+      { "name"=>"graphite", "release_versions"=>[{"version"=>"0d79bf5aa5f2cf29195bff725d7dee55dea1aedc"}] },
+      { "name"=>"logsearch", "release_versions"=>[{"version"=>"23.0.0"}] },
+      { "name"=>"logsearch-for-cloudfoundry", "release_versions"=>[{"version"=>"7"}] },
+      { "name"=>"nginx", "release_versions"=>[{"version"=>"2"}] },
+    ]
   }
+  let(:mock_bosh_stemcells) {
+    [{"name"=>"bosh-aws-xen-hvm-ubuntu-trusty-go_agent", "version"=>"3074"}]
+  }
+
+  it "should be valid" do
+    mock_director = double("Director", :uuid => 'BOSH_UUID')
+    allow(mock_director).to receive(:list_releases).and_return(mock_bosh_releases)
+    allow(mock_director).to receive(:list_stemcells).and_return(mock_bosh_stemcells)
+
+    m = Bosh::Cli::Manifest.new(manifest_file, mock_director)
+    m.load
+
+    expect { m.validate }.not_to raise_error
+  end
 
   it "should contain 5 jobs" do
     expect(manifest["jobs"].size).to eq(33)
