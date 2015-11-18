2e246cbae318bd711d3d974bc9df6e3ab28a4d7c
Piotr Komborski <piotr.komborski@gmail.com>
2015-11-18 14:19:38 +0000

    Testing variable passing to docker container

--- run_deployment.sh
+++ run_deployment.sh
@@ -1,6 +1,10 @@
 #! /bin/bash
 
+export TF_VAR_AWS_ACCESS_KEY_ID=$1
+export TF_VAR_AWS_SECRET_ACCESS_KEY=$2
+
 DIR=$(dirname $0)
 cd $DIR
 
-make aws DEPLOY_ENV=piotr
+#make aws DEPLOY_ENV=piotr
+env
