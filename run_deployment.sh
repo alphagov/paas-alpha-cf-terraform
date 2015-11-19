52294889abb7f7e185ef3e3fc9a92d3aebfe82fb
Piotr Komborski <piotr.komborski@gmail.com>
2015-11-19 12:24:02 +0000

    We need mor keys :)

--- run_deployment.sh
+++ run_deployment.sh
@@ -5,6 +5,7 @@ echo "192.168.9.110 github.gds" >> /etc/hosts
 mkdir -p ~/.ssh/
 chmod 600 ~/.ssh
 echo $JENKINS_SSH_KEY > ~/.ssh/id_rsa.pub
+echo $JENKINS_PRIVATE_SSH_KEY > ~/.ssh/id_rsa.pub
 echo "github.gds,192.168.9.110 ecdsa-sha2-nistp256 AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBLN3zohMrxpugJsxfy7Js+e75jVAm1xhiHTD7+GUaLMxGbp9oDxxvctS0xY+hvi7PWU/SUnU2AaShZf21HXARXE=" >> ~/.ssh/known_hosts
 chmod 400 ~/.ssh/id_rsa.pub
 git clone jenkins@git@github.gds:multicloudpaas/credentials.git ~/.paas-pass
@@ -15,7 +16,7 @@ DIR=$(dirname $0)
 cd $DIR
 
 echo Setting up ssh-agent and cleanup trap
-echo $SSH_KEY > ~/.ssh/insecure-deployer
+echo $INSECURE_DEPLOYER_SSH_KEY > ~/.ssh/insecure-deployer
 chmod 400 ~/.ssh/insecure-deployer
 eval $(ssh-agent) && ssh-add ~/.ssh/insecure-deployer
 set -e
