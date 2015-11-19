0d7c8359b7c507ca5d642052e132ba4861dd6cc9
Timothy Mower <timothy.mower@gmail.com>
2015-11-19 15:15:18 +0000

    wip

--- run_deployment.sh
+++ run_deployment.sh
@@ -1,12 +1,14 @@
 #! /bin/bash
 
 echo "192.168.9.110 github.gds" >> /etc/hosts
-mkdir -p ~/.ssh/
-chmod 600 ~/.ssh
+mkdir -p ~/.ssh/ ~/.gnupg
+chmod 600 ~/.ssh ~/.gnupg
 echo "$JENKINS_SSH_KEY" > ~/.ssh/id_rsa.pub
 echo "$JENKINS_PRIVATE_SSH_KEY" > ~/.ssh/id_rsa
 echo "$INSECURE_DEPLOYER_SSH_KEY" > ~/.ssh/insecure-deployer.pub
 echo "$INSECURE_DEPLOYER_PRIVATE_SSH_KEY" > ~/.ssh/insecure-deployer
+echo "$JENKINS_GPG_KEY" > ~/.gnupg/multicloud-deploy.key
+gpg --batch --yes --allow-secret-key-import --import ~/.gnupg/multicloud-deploy.key
 echo "github.gds,192.168.9.110 ecdsa-sha2-nistp256 AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBLN3zohMrxpugJsxfy7Js+e75jVAm1xhiHTD7+GUaLMxGbp9oDxxvctS0xY+hvi7PWU/SUnU2AaShZf21HXARXE=" >> ~/.ssh/known_hosts
 chmod 400 ~/.ssh/id_rsa.pub ~/.ssh/id_rsa ~/.ssh/insecure-deployer.pub ~/.ssh/insecure-deployer
 git clone git@github.gds:multicloudpaas/credentials.git ~/.paas-pass
@@ -20,6 +22,6 @@ echo Setting up ssh-agent and cleanup trap
 eval $(ssh-agent) && ssh-add ~/.ssh/insecure-deployer
 mkdir -p aws/ssh
 cp ~/.ssh/* aws/ssh
-make aws DEPLOY_ENV=piotr ${EXTRA_OPTIONS}
+make aws DEPLOY_ENV=piotr ${EXTRA_OPTIONS} ROOT_PASS_DIR=jenkins
 set -e
 trap "cd aws;terraform destroy -state=piotr.tfstate -var env=piotr -var force_destroy=true >/dev/null 2>&1 " ERR
