2ed660326f03de8f00c630f2b34ce1069ce9e084
Jim Conner <jim.conner@digital.cabinet-office.gov.uk>
2016-01-08 15:08:53 +0000

    Configure ELB and security groups for cf ssh proxy
    This commit creates a dedicated ELB and security group for ssh-proxy
    access as sharing one with the web access leads to traffic being
    incorrectly routed. The terraform outputs have been extended to include
    the new sshproxy elb, which is then used in the deployment manifests.

--- aws/dns-records.tf
+++ aws/dns-records.tf
@@ -6,6 +6,14 @@ resource "aws_route53_record" "wildcard" {
   records = ["${aws_elb.router.dns_name}"]
 }
 
+resource "aws_route53_record" "sshproxy" {
+  zone_id = "${var.dns_zone_id}"
+  name = "ssh.${var.env}.${var.dns_zone_name}."
+  type = "CNAME"
+  ttl = "60"
+  records = ["${aws_elb.ssh-proxy-router.dns_name}"]
+}
+
 resource "aws_route53_record" "bastion" {
   zone_id = "${var.dns_zone_id}"
   name = "${var.env}-bastion.${var.dns_zone_name}."
